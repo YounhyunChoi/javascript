b1ba2d2656aacd5170d44c667de014dba716f19d
yunghyunchoi <asl321@naver.com>
2023-02-03 17:49:56 +0900

    a

---
+++ 2.function/5.4.scope.js
@@ -0,0 +1,16 @@
+let userName = 'neo'
+sayHi()
+
+function sayHi() {
+    console.log('Hi', userName)
+}
+
+sayHi()
+
+sayHello()
+
+const sayHello = function() {
+    console.log('Hello', userName)
+}
+
+sayHello()
