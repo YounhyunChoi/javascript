404cd0727059d2e9d80d5fe632df146096acbe6a
yunghyunchoi <asl321@naver.com>
2023-02-13 12:22:44 +0900

    a

---
+++ 9.typescript/1.variable.js
@@ -0,0 +1,24 @@
+var msg = 'hello';
+// msg = 1
+var val = 1;
+var nullableStr = null;
+nullableStr = 'Hi';
+// nullableStr = undefined
+var undefinedOrNumber;
+undefinedOrNumber = 10;
+undefinedOrNumber = undefined;
+var numberOrStringOrNull = null;
+numberOrStringOrNull = 1;
+numberOrStringOrNull = 'Bye';
+var isCompleted = true;
+isCompleted = false;
+// isCompleted = 0
+var anyValue = null;
+anyValue = undefined;
+anyValue = 1;
+anyValue = 'hello';
+var a = 1;
+var b = 'b';
+var c = true;
+
+ECMAscript6
